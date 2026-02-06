eb6d1cddb5f0b0866b4471fe77afeae75859b833
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2026-02-06 11:29:11 +0100

    definida estructura del proyecto ahora si

---
+++ BurgerWeb_Enero/src/App.tsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// Componentes globales
+import {Header} from './components/Header';
+import {Footer} from './components/Footer';
+
+// Páginas (Asegúrate de que estas rutas de archivo sean correctas)
+import Home from './pages/Home';
+import Burgers from './pages/Burgers'; 
+import BurgerDetail from './pages/BurgerDetail'; 
+import FoodTruckDetail from './pages/FoodTruckDetail'; 
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app-layout">
+        <Header />
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/burgers" element={<Burgers />} />
+          <Route path="burgers/:id" element={<BurgerDetail />} />
+          <Route path="/foodtrucks/:id" element={<FoodTruckDetail />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App;
